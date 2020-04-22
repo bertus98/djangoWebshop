cf3b77e7494765d0c8908938bb8cfa6fd373f87c
bertus98 <bertus1998@hotmail.com>
2020-04-22 14:03:02 +0200

    add models class products

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class products(models.Model):
+    productName = models.CharField(max_length=300)
+    productPrice = models.CharField(max_length=20)
+    productDescription = models.TextField()
+    productImg1 = models.Charfield(max_length=300) 
+    productImg2 = models.Charfield(max_length=300) 
+    productImg3 = models.Charfield(max_length=300) 
